a1b2377499bb3e5ca22059ba8d5e635ae3e63912
Sagar Jain <sagar@zitto.com>
2020-01-09 12:09:53 +0530

    fix: lighthouse audit fixes

--- js/main.js
+++ js/main.js
@@ -296,7 +296,7 @@
 			event.preventDefault();
 
 			$('html,body').animate({
-				scrollTop: $('#about-section').offset().top - 180
+				scrollTop: $('#about-section').offset().top - 70
 			}, 500, 'easeInOutExpo');
 			
 			return false;
